5013bdd5ce725d74681149da89d3f7a5e029c1eb
miracle0930 <miracle0930@163.com>
2018-04-12 16:19:53 -0500

    Unit Test added

--- FlickrGallery/FlickrGalleryTests/FlickrGalleryTests.swift
+++ FlickrGallery/FlickrGalleryTests/FlickrGalleryTests.swift
@@ -7,29 +7,50 @@
 //
 
 import XCTest
+@testable import FlickrGallery
+var testAPIParser: FlickrAPIParser!
+var testGalleryCollectionViewController: GalleryCollectionViewController!
 
 class FlickrGalleryTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        testAPIParser = FlickrAPIParser()
+        testGalleryCollectionViewController = GalleryCollectionViewController()
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        testAPIParser = nil
+        testGalleryCollectionViewController = nil
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    // test the correct return numbers of photo in correct range
+    func testGeneralPageOnFlickrAPI() {
+        print("Start 'GeneralPageOnFlickrAPI Test.'")
+        let page = 1
+        let per_page = 13
+        for _ in stride(from: page, to: 40, by: 1) {
+            let exp: XCTestExpectation? = expectation(description: "timeout!")
+            testAPIParser.getInterestingnessList(key: api_key, date: "2018-04-11", page: page, per_page: per_page, completion: { (photos) in
+                let ans = page * per_page > 500 ? 500 - (page - 1) * per_page : per_page
+                XCTAssertEqual(photos.count, ans, "Didn't return correct number of photos.")
+                exp?.fulfill()
+            })
+            waitForExpectations(timeout: 3) { (error) in
+                if let _ = error {
+                    print("timeout!")
+                }
+            }
         }
     }
     
+    // nothing should be shown when the maximum number of photos reached
+    func testLoadMoreWhenPageIsLargerThanTotalPage() {
+        let page = 40
+        let per_page = 13
+        let testCount = testGalleryCollectionViewController.photos.count
+        testAPIParser.getInterestingnessList(key: api_key, date: "2018-04-12", page: page, per_page: per_page) { (_) in}
+        XCTAssertEqual(testCount, 0, "Page 40 is greater than the largest page, nothing should be added into photos")
+    }
 }
